Ryan-B-W/vimacs.app

commit 1161471892c722ec1038376f3d52da03dd3e612f

Put built-in treesit and eglot configuration behind feature checks

init.el
@@ -501,6 +501,7 @@
 (use-package treesit
   :pin manual
   :ensure nil
+  :if (featurep 'treesit)
   :init
   (setf treesit-language-source-alist
         '((bash "https://github.com/tree-sitter/tree-sitter-bash")
@@ -558,6 +559,7 @@
 (use-package eglot
   :pin manual
   :ensure nil
+  :if (featurep 'eglot)
   :hook ((bash-ts-mode . eglot-ensure)
          (c++-mode . eglot-ensure)
          (c-or-c++-mode . eglot-ensure)

commit bfcc62519f90fdc339096a32070a0c76c794755e

Use fboundp check of autoloaded function for treesit and eglot
This fixes a regression introduced in commit
1161471892c722ec1038376f3d52da03dd3e612f due to checking for the
treesit and eglot features before they get provided and therefore not
loading them.

init.el
@@ -506,7 +506,7 @@
 (use-package treesit
   :pin manual
   :ensure nil
-  :if (featurep 'treesit)
+  :if (fboundp 'treesit-install-language-grammar)
   :init
   (setf treesit-language-source-alist
         '((bash "https://github.com/tree-sitter/tree-sitter-bash")
@@ -564,7 +564,7 @@
 (use-package eglot
   :pin manual
   :ensure nil
-  :if (featurep 'eglot)
+  :if (fboundp 'eglot)
   :hook ((bash-ts-mode . eglot-ensure)
          (c++-mode . eglot-ensure)
          (c-or-c++-mode . eglot-ensure)