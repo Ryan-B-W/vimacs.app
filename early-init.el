54803fbd3ec9eecfd54762d034022d8eb0d89761
Ryan Wilcox <ryanbw@sdf.org>
2023-12-09 05:25:52 -0800

    Add early-init.el with early startup UI tweaks

---
+++ early-init.el
@@ -0,0 +1,4 @@
+;; Do some early tweaks to the UI before starting.
+(setf initial-frame-alist '((background-color . "#141617")
+                            (vertical-scroll-bars . nil)
+                            (horizontal-scroll-bars . nil)))
